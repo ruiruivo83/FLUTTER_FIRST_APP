96b19c288b3ebe23cb56a403ee8895b696841c2f
Rui Ruivo <ruivo.rui@gmail.com>
2023-05-24 16:23:16 +0200

    Project creation
    Main in:  "lib/main.dart"

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // MaterialApp() - Creates a widget
+  // Can talke positional or named arguments
+  runApp(const MaterialApp(home: Text("Hello World")));
+}
