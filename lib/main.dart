339a7b51496a0d83c16310db21b3c52cc5e28e06
Rui Ruivo <ruivo.rui@gmail.com>
2023-05-24 16:42:02 +0200

    Implement Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
   // MaterialApp() - Creates a widget
   // Can talke positional or named arguments
-  runApp(const MaterialApp(home: Text("Hello World")));
+  // Use Scaffold to implement a basic style
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    ),
+  );
 }
