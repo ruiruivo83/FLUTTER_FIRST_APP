64a08bc1db222ebd5e1bc84792124310106a419d
Rui Ruivo <ruivo.rui@gmail.com>
2023-05-25 10:06:45 +0200

    Text Style

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,36 @@ void main() {
   // Can talke positional or named arguments
   // Use Scaffold to implement a basic style
   runApp(
-    const MaterialApp(
+    // const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        // backgroundColor: Color.fromARGB(255, 159, 133, 202),
+        body: Container(
+          // Container does not support "const"
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(255, 238, 255, 0),
+                Color.fromARGB(255, 255, 255, 255)
+              ],
+            ),
+          ),
+          child: Center(
+            child: RichText(
+              text: const TextSpan(
+                text: 'Hello World',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.black,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.red,
+                  decorationStyle: TextDecorationStyle.wavy,
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     ),
